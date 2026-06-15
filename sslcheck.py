3c773bec6f303d88c0dba40002b3105ea339b214
Ondrej Bima <ondra@bimovi.cz>
2026-06-15 20:18:44 +0200

    final

--- sslcheck.py
+++ sslcheck.py
@@ -28,7 +28,7 @@ from rich.console import Console
 from rich.panel import Panel
 from rich.table import Table
 
-console = Console()
+console = Console(emoji=False)
 
 
 # ---------------------------------------------------------------------------
